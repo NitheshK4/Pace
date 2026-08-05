e8700d3af56d1f09cdd44fd3c468f74758c3630a
Nithesh kumar S <nitheshk236@gmail.com>
2026-08-05 20:23:16 +0530

    feat(examples): implement custom getPath method in Symfony PaceBundle

--- examples/php-symfony/PaceBundle.php
+++ examples/php-symfony/PaceBundle.php
@@ -5,4 +5,7 @@ namespace App\PaceBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PaceBundle extends Bundle {
+    public function getPath(): string {
+        return \dirname(__DIR__);
+    }
 }
